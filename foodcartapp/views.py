72e4dbc5fcfb9dc27e4eb28435013f517f73633f
Ars <xakars02@gmail.com>
2023-01-04 23:01:05 +0600

    Extend data validation for register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -3,6 +3,7 @@ from django.templatetags.static import static
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+import phonenumbers
 
 
 from .models import Product
@@ -65,16 +66,36 @@ def product_list_api(request):
 @api_view(['POST'])
 def register_order(request):
     client_order = request.data
-
-    if 'products' not in client_order:
-        content = {'error': '"products" is required parameter'}
-        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    print(client_order)
+    keys = {'products', 'firstname', 'lastname', 'phonenumber', 'address'}
+    if not keys <= client_order.keys():
+        content = {'error': '"products", "firstname", "lastname", "phonenumber", "address" are required parameters'}
+        return Response(content, status=400)
     if not isinstance(client_order['products'], list):
         content = {'error': f'expected list instead of {type(client_order["products"])}'}
-        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        return Response(content, status=400)
     if not client_order['products']:
         content = {'error': 'product list is empty'}
-        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        return Response(content, status=400)
+
+    if not (client_order['firstname'] and client_order['lastname'] and client_order['phonenumber'] and client_order['address']):
+        content = {'error': '"firstname", "lastname", "phonenumber", "address" must not be empty'}
+        return Response(content, status=400)
+
+    phone_number = phonenumbers.parse(client_order['phonenumber'], 'RU')
+    if not phonenumbers.is_valid_number(phone_number):
+        content = {'error': 'phone_number is not valid'}
+        return Response(content, status=400)
+
+    products_id = Product.objects.available().values_list('id', flat=True)
+    orders_id = {product['product'] for product in client_order['products']}
+    if not orders_id.issubset(products_id):
+        content = {'error': 'no such product'}
+        return Response(content, status=400)
+
+    if not isinstance(client_order['firstname'], str):
+        content = {'error': 'first name must be string'}
+        return Response(content, status=400)
 
     order = Order.objects.create(
         address=client_order['address'],
