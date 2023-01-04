12685ababa1e2a612d0e78c392a174db5aa30d05
Ars <xakars02@gmail.com>
2023-01-05 02:11:17 +0600

    Rewrite data validation logic with DRF

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,14 +1,36 @@
 from django.http import JsonResponse
 from django.templatetags.static import static
+import phonenumbers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
-import phonenumbers
-
+from rest_framework.serializers import Serializer
+from rest_framework.serializers import CharField, ListField, DictField
+from rest_framework.serializers import ValidationError
 
 from .models import Product
 from .models import Order
 from .models import OrderDetail
+
+
+class OrserSerializer(Serializer):
+    products = ListField(child=DictField(), allow_empty=False)
+    firstname = CharField()
+    lastname = CharField()
+    phonenumber = CharField()
+    address = CharField()
+
+    def validate_phonenumber(self, value):
+        phone_number = phonenumbers.parse(value, 'RU')
+        if not phonenumbers.is_valid_number(phone_number):
+            raise ValidationError(['Phone number field is not valid'])
+        return value
+
+    def validate_products(self, value):
+        products_id = Product.objects.available().values_list('id', flat=True)
+        orders_id = {product['product'] for product in value}
+        if not orders_id.issubset(products_id):
+            raise ValidationError([f'Недопустимый первичный ключ {orders_id.difference(products_id)}'])
+        return value
 
 
 def banners_list_api(request):
@@ -66,36 +88,8 @@ def product_list_api(request):
 @api_view(['POST'])
 def register_order(request):
     client_order = request.data
-    print(client_order)
-    keys = {'products', 'firstname', 'lastname', 'phonenumber', 'address'}
-    if not keys <= client_order.keys():
-        content = {'error': '"products", "firstname", "lastname", "phonenumber", "address" are required parameters'}
-        return Response(content, status=400)
-    if not isinstance(client_order['products'], list):
-        content = {'error': f'expected list instead of {type(client_order["products"])}'}
-        return Response(content, status=400)
-    if not client_order['products']:
-        content = {'error': 'product list is empty'}
-        return Response(content, status=400)
-
-    if not (client_order['firstname'] and client_order['lastname'] and client_order['phonenumber'] and client_order['address']):
-        content = {'error': '"firstname", "lastname", "phonenumber", "address" must not be empty'}
-        return Response(content, status=400)
-
-    phone_number = phonenumbers.parse(client_order['phonenumber'], 'RU')
-    if not phonenumbers.is_valid_number(phone_number):
-        content = {'error': 'phone_number is not valid'}
-        return Response(content, status=400)
-
-    products_id = Product.objects.available().values_list('id', flat=True)
-    orders_id = {product['product'] for product in client_order['products']}
-    if not orders_id.issubset(products_id):
-        content = {'error': 'no such product'}
-        return Response(content, status=400)
-
-    if not isinstance(client_order['firstname'], str):
-        content = {'error': 'first name must be string'}
-        return Response(content, status=400)
+    serializer = OrserSerializer(data=client_order)
+    serializer.is_valid(raise_exception=True)
 
     order = Order.objects.create(
         address=client_order['address'],
