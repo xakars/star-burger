a89c2bb8bf41f2455248ee0b7ad57461537327b1
Ars <xakars02@gmail.com>
2023-01-12 22:55:40 +0600

    add a return to the order page

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.shortcuts import redirect
+from django.utils.http import url_has_allowed_host_and_scheme
+
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
@@ -121,3 +124,13 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = [
         OrderDetailInline
     ]
+
+    def response_change(self, request, obj):
+        response = super(OrderAdmin, self).response_change(request, obj)
+        if 'next' in request.GET:
+            if url_has_allowed_host_and_scheme(request.GET['next'], None):
+                return redirect(request.GET['next'])
+            else:
+                raise
+        else:
+            return response
