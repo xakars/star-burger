30b1175ab3bdde7b52e9d3f695d521b438fa7237
remote_lndn <xakars02@gmail.com>
2023-06-25 16:45:49 +0000

    add docker-compose, nginx container

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn star_burger.wsgi:application -b 0.0.0.0:8179 
+gunicorn star_burger.wsgi:application -b 0.0.0.0:8000 
--- star_burger/settings.py
+++ star_burger/settings.py
@@ -82,7 +82,14 @@ WSGI_APPLICATION = 'star_burger.wsgi.application'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-DATABASES = {'default': dj_database_url.config()}
+#DATABASES = {'default': dj_database_url.config()}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
